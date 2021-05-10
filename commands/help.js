bc06fd026534934692bd2ad2defbdd5a9cc6ff70
Nicolas Ferlet <nicolas.ferlet@infolegale.fr>
2021-05-10 14:37:52 +0200

    Help command looks good now

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,14 @@ const { prefix } = require('../config.json');
 
 function help(message, args, client) {
     let embed = new MessageEmbed()
-		.setTitle('Help')
-		.setDescription('Command help for ElswordRaidManager bot')
-		.setFooter(prefix + 'help command (command2 ...) for informations about specific commands');
+		.setAuthor('Rosso raid manager', 'https://64.media.tumblr.com/4d56ac1bcd708c38392a6b37f98a68b8/tumblr_pozk3r9eiW1wsn58z_640.jpg')
+		.setThumbnail('https://cdn.discordapp.com/attachments/736163626934861845/742671714386968576/help_animated_x4_1.gif')
+		.setTitle('Command help')
+		.setDescription('Type ``' + prefix + 'help [command]`` for more help eg. ``' + prefix + 'help add``')
+		.addField('Characters', '``add`` ``remove``\n``set`` ``run``\n``move``', true)
+		.addField('List', '``list`` ``show``\n``weeklyreset``', true)
+		.addField('Misc.', '``help``', true);
 
-    client.commands.forEach(function(value, key) {
-        embed.addField(value.name, value.description);
-    })
     helper.sendBotMessage(message, embed);
 }
 
