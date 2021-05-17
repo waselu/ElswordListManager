de3ca234b606a5a16aa3976eb547b55d635ac25d
Nicolas Ferlet <nicolas.ferlet@infolegale.fr>
2021-05-17 11:02:38 +0200

    move command works with multiple lists + new dedbug command

---
+++ commands/logList.js
@@ -0,0 +1,21 @@
+const saveManager = require('../utils/saveManager');
+
+async function logList(message, args, client) {
+    if (args.length > 0) {
+        for (arg of args) {
+            console.log(saveManager.getList()[message.author.username]['lists'][arg]['list']);
+        }
+    } else {
+        console.log(saveManager.getList()[message.author.username]);
+    }
+}
+
+module.exports = {
+	name: 'loglist',
+	description: '',
+    example: '',
+    additionalInfo: '',
+	async execute(message, args, client) {
+		await logList(message, args, client);
+	}
+}
--- commands/move.js
+++ commands/move.js
@@ -7,7 +7,7 @@ async function move(message, args) {
     let list = saveManager.getList();
 
     function getActualIndexMoveTo(indexMoveTo, user) {
-        let charList = list[user];
+        let charList = list[user]['lists'][list[user]['active']]['list'];
         let mustAdd = 0;
         indexMoveTo = indexMoveTo;
         for ([index, classDef] of charList.entries()) {
@@ -41,14 +41,14 @@ async function move(message, args) {
     for ([index, className] of classNames.entries()) {
         let indexMoveTo = positions[index];
         async function classFound(message, index, realName) {
-            if (indexMoveTo <= 0 || indexMoveTo > list[message.author.username].length) {
+            if (indexMoveTo <= 0 || indexMoveTo > list[message.author.username]['lists'][list[message.author.username]['active']]['list'].length) {
                 await helper.sendBotMessage(message, "Index " + indexMoveTo + " isn't in your list");
                 return;
             }
             indexMoveTo = getActualIndexMoveTo(indexMoveTo, message.author.username);
-            let item = list[message.author.username][index];
-            list[message.author.username].splice(index, 1);
-            list[message.author.username].splice(indexMoveTo - 1, 0, item);
+            let item = list[message.author.username]['lists'][list[message.author.username]['active']]['list'][index];
+            list[message.author.username]['lists'][list[message.author.username]['active']]['list'].splice(index, 1);
+            list[message.author.username]['lists'][list[message.author.username]['active']]['list'].splice(indexMoveTo - 1, 0, item);
         }
 
         async function classNotFound(message, realName) {
