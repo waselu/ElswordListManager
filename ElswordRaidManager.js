33f08502481f21834e6530ed12fbf11377052b25
Nicolas Ferlet <nicolas.ferlet@infolegale.fr>
2021-06-04 12:18:52 +0200

    changed todo

--- ElswordRaidManager.js
+++ ElswordRaidManager.js
@@ -18,7 +18,7 @@ require('discord-buttons')(client);
 //Rework client being passed everywhere, direcly call command via require
 //Remove setconfig and helpconfig commands, add editconfig command that behave like the edit command (buttons)
 //Add other list behavior to the edit command
-//Sort each list independently when autosorting
+//Sort each list independently according to character when autosorting
 
 //Fixes
 //Add JSON.parse(JSON.stringify)
