cd1f83ba79be535ea4b5e0967058d9331e8bc1ff
Nicolas Ferlet <nicolas.ferlet@infolegale.fr>
2021-06-09 12:06:01 +0200

    changed todos

--- ElswordRaidManager.js
+++ ElswordRaidManager.js
@@ -16,14 +16,14 @@ require('discord-buttons')(client);
 //Mandatory
 //Add list types: SD (Later: Berthe)
 //Disable buttons instead of removing message
-//Check if other users can interact with buttons
 
 //Long term
-//Merge edit and move commands
-//Merge edit and config commands
+//Merge edit and move commands (Add list of all characters to edit command)
+//Merge edit and config commands (Maybe not)
 
 //Clear logs older than 1 month (parse log date)
 //Anti-conflict system on logs, 10+ files and array of used files to use an unused one, store time of command to merge afterward
+//Discord slash commands
 
 client.commands = new Discord.Collection();
 
